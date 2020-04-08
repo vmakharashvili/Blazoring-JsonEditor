a165df8f73ae909daaefc94854d06d95adc0a801
Vasil Makharashvili <vmakharashvili@gmail.com>
2020-04-08 18:35:19 +0400

    row data

---
+++ Blazoring.JsonEditor/Blazoring.JsonEditor.Demo/Models/JsonTestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazoring.JsonEditor.Demo.Models
+{
+    public class JsonTestModel
+    {
+        public string Name { get; set; }
+        public string JsonValue { get; set; }
+    }
+}
