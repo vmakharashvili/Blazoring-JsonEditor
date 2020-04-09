1d94e2a62b71fedbb4d13ea9945a7d373f048ca1
Vasil Makharashvili <vmakharashvili@gmail.com>
2020-04-09 17:28:18 +0400

    publish

---
+++ Blazoring.JsonEditor/Blazoring.JsonEditor.Demo/dist/wwwroot/_content/Blazoring.JsonEditor/Blazoring.JsonEditor.js
@@ -0,0 +1,9 @@
+﻿window.CopyToClipboard = (txt) => {
+    var el = document.createElement('textarea');
+    el.value = txt;
+    el.setAttribute('readonly', '');
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
