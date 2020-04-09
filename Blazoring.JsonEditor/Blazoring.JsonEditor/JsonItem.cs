b11d563c02df6f83d5e2f39391f96c493ffe4d5d
Vasil Makharashvili <vmakharashvili@gmail.com>
2020-04-09 14:36:24 +0400

    123

---
+++ Blazoring.JsonEditor/Blazoring.JsonEditor/JsonItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json;
+
+namespace Blazoring.JsonEditor
+{
+    public class JsonItem
+    {
+        [RequiredIf(nameof(JsonItem.ValueKind), JsonValueKind.Undefined, JsonValueKind.Number, JsonValueKind.String, JsonValueKind.Object, JsonValueKind.Array, JsonValueKind.True)]
+        public string? PropertyName { get; set; }
+
+        [RequiredIf(nameof(JsonItem.ValueKind), JsonValueKind.String, JsonValueKind.Array, JsonValueKind.False, JsonValueKind.Null)]
+        public string? Value { get; set; }
+
+        [RequiredIf(nameof(JsonItem.ValueKind), JsonValueKind.Number)]
+        public double? NumericValue { get; set; }
+
+        [RequiredIf(nameof(JsonItem.Value), JsonValueKind.True)]
+        public bool? BooleanValue { get; set; }
+        public JsonValueKind ValueKind { get; set; }
+
+    }
+}
---
+++ Blazoring.JsonEditor/Blazoring.JsonEditor/RequiredIfAttribute.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace Blazoring.JsonEditor
+{
+    //public class RequiredIfAttribute : ValidationAttribute
+    //{
+    //    RequiredAttribute _innerAttribute = new RequiredAttribute();
+    //    public string _dependentProperty { get; set; }
+    //    public Func<bool> _condition { get; set; }
+
+    //    public RequiredIfAttribute(string dependentProperty, Func<bool> condition)
+    //    {
+    //        this._dependentProperty = dependentProperty;
+    //        this._condition = condition;
+    //    }
+    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    //    {
+    //        var field = validationContext.ObjectType.GetProperty(_dependentProperty);
+    //        if (field != null)
+    //        {
+    //            var dependentValue = field.GetValue(validationContext.ObjectInstance, null);
+    //            if ((dependentValue == null && _targetValue == null) || (dependentValue != null && dependentValue.Equals(_targetValue)))
+    //            {
+    //                if (!_innerAttribute.IsValid(value))
+    //                {
+    //                    string name = validationContext.DisplayName;
+    //                    return new ValidationResult(ErrorMessage = name + " Is required.");
+    //                }
+    //            }
+    //            return ValidationResult.Success;
+    //        }
+    //        else
+    //        {
+    //            return new ValidationResult(FormatErrorMessage(_dependentProperty));
+    //        }
+    //    }
+    //}
+
+    public class RequiredIfAttribute : ValidationAttribute
+    {
+        RequiredAttribute _innerAttribute = new RequiredAttribute();
+        public string _dependentProperty { get; set; }
+        public List<object> _targetValues { get; set; }
+
+
+        public RequiredIfAttribute(string propertyName, params object[] dependents)
+        {
+            _dependentProperty = propertyName;
+            _targetValues = dependents.ToList();
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var field = validationContext.ObjectType.GetProperty(_dependentProperty);
+            if (field != null)
+            {
+                var dependentValue = field.GetValue(validationContext.ObjectInstance, null);
+                if (_targetValues?.Any(x => x.Equals(dependentValue)) == true)
+                {
+                    _innerAttribute.ErrorMessage = "Required!";
+                    return _innerAttribute.GetValidationResult(value, validationContext);
+                }
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(FormatErrorMessage(_dependentProperty));
+            }
+        }
+    }
+}
